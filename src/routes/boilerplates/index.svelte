cad760542478a6d72548dc638b220810f87635c6
Ambar Mutha <ambar.ytl@gmail.com>
2021-08-03 00:31:11 +0530

    Fix: controls are misaligned and missing arrow icon

--- src/routes/boilerplates/index.svelte
+++ src/routes/boilerplates/index.svelte
@@ -47,6 +47,10 @@
     @apply text-5xl;
   }
 
+  hr {
+    margin-block: 4rem;
+  }
+
   .controls {
     display: flex;
     justify-content: space-between;
@@ -57,7 +61,7 @@
 
   .inputs {
     display: grid;
-    grid-template-columns: repeat(3, auto);
+    grid-template-columns: repeat(4, auto);
     grid-gap: 0.5rem;
     margin-right: 2rem;
   }
