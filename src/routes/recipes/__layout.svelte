717a96590df01db21408b24fa67c47d050bff794
Ambar Mutha <ambar.ytl@gmail.com>
2021-09-09 14:37:57 +0530

    Add code syntax highlighting (hljs)

--- src/routes/recipes/__layout.svelte
+++ src/routes/recipes/__layout.svelte
@@ -1,45 +1,47 @@
 <script lang="ts" context="module">
-  import { categories } from '$lib/stores/recipes';
+	import { categories } from '$lib/stores/recipes';
+	import '$styles/highlight.css';
 
-  export async function load({ fetch }) {
-    const res = await fetch('/recipes/recipes');
-    const recipeCategories = await res.json();
+	export async function load({ fetch }) {
+		const res = await fetch('/recipes/recipes');
+		const recipeCategories = await res.json();
 
-    if (!res.ok) {
-      return {
-        status: res.status,
-        error: new Error()
-      };
-    }
+		if (!res.ok) {
+			return {
+				status: res.status,
+				error: new Error()
+			};
+		}
 
-    categories.set(recipeCategories);
-    return { props: { categories: recipeCategories } };
-  }
+		categories.set(recipeCategories);
+		return { props: { categories: recipeCategories } };
+	}
 </script>
 
 <svelte:head>
-  <title>Recipes - Svelte Society</title>
+	<title>Recipes - Svelte Society</title>
 </svelte:head>
 
 <div class="container">
-  <slot />
+	<slot />
 </div>
 
 <style>
-  :global(article blockquote) {
-    background: rgba(255, 62, 1, 0.2);
-    border-radius: 5px 0px 0px 5px;
-    color: black;
-    border-left: 2px solid #ff3e01;
-  }
-  .container :global(h2), .container :global(h3) {
-    margin-top: 2rem;
-    margin-bottom: 1.25rem;
-  }
-  .container :global(p) {
-    margin-bottom: 1.25rem;
-  }
-  .container :global(li) {
-    margin-bottom: 1.1rem;
-  }
+	:global(article blockquote) {
+		background: rgba(255, 62, 1, 0.2);
+		border-radius: 5px 0px 0px 5px;
+		color: black;
+		border-left: 2px solid #ff3e01;
+	}
+	.container :global(h2),
+	.container :global(h3) {
+		margin-top: 2rem;
+		margin-bottom: 1.25rem;
+	}
+	.container :global(p) {
+		margin-bottom: 1.25rem;
+	}
+	.container :global(li) {
+		margin-bottom: 1.1rem;
+	}
 </style>
