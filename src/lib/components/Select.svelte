738d334d9abf5d988ec5b11a4759ee34c15f0fc7
Martin Skočík <mskocik@gmail.com>
2021-12-05 15:14:55 +0100

    update svelecte styles based on component css variables

--- src/lib/components/Select.svelte
+++ src/lib/components/Select.svelte
@@ -9,7 +9,7 @@
 	{#if label}
 		<span>{label}</span>
 	{/if}
-	<Svelecte class="themed-svelecte" bind:value {...$$restProps} />
+	<Svelecte bind:value {...$$restProps} />
 </div>
 
 <style>
@@ -21,17 +21,28 @@
 		display: flex;
 		flex-direction: column;
 	}
-
-	.themed :global(.sv-control) {
-		min-height: 62px;
-		border: 2px solid var(--dark-gray);
+	.themed :global(.svelecte-control) {
+		--sv-min-height: 62px;
+		--sv-border: 2px solid var(--dark-gray);
+		--sv-active-border: 2px solid var(--secondary);
+		--sv-item-active-bg: var(--secondary);
+		--sv-item-active-color: #fff;
+		--sv-item-btn-icon: #fff;
+		--sv-item-selected-bg: var(--secondary);
+		--sv-item-btn-bg: var(--secondary);
+		--sv-item-btn-bg-hover: #b32d00;
 	}
-	.themed :global(.sv-control:hover) {
-		border: 2px solid var(--secondary);
-	}
-	.themed :global(.sv-content) {
+	.themed :global(.svelecte-control .sv-content) {
 		padding: calc(13.5px - 4px) var(--s-2);
 	}
+	.themed :global(.svelecte-control .sv-content.has-multiSelection) {
+		--sv-item-color: #fff;
+	}
+	.themed :global(.sv-item) {
+		border-radius: 5px;
+		overflow: hidden;
+	}
+
 	.themed :global(.sv-control .indicator-container svg) {
 		width: 36px;
 		height: 36px;
@@ -39,9 +50,6 @@
 	}
 	.themed :global(.sv-control:hover .indicator-container svg) {
 		fill: var(--secondary);
-	}
-	.themed :global(.sv-item) {
-		border-radius: 4px;
 	}
 	.themed :global(a.sv-item-btn) {
 		border-width: 0;
