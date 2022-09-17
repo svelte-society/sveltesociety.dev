ad2513508d9ff57ee48f9859588389976a418ebb
Michael <michael.spengler@sap.com>
2022-09-17 21:39:21 +0200

    automate cicd for github pages based deployment - adding docs folder...

---
+++ docs/_app/immutable/chunks/10-5a660a31.js
@@ -0,0 +1,7 @@
+import { default as a } from '../components/pages/events/frsocietyday2020/_page.svx-c7f975d8.js';
+import './index-2fe5515f.js';
+import './EventPage-ede45e7c.js';
+import './Seo-17c7a174.js';
+import './stores-fefb1cb6.js';
+import './singletons-86285715.js';
+export { a as component };
