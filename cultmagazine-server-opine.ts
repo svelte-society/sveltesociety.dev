32cfcf46563f0374a3eb1a5fe9fedd7bb016e7e5
Michael <michael.spengler@sap.com>
2022-09-18 11:57:32 +0200

    add server - Article

---
+++ cultmagazine-server-opine.ts
@@ -0,0 +1,13 @@
+import { opine, serveStatic } from 'https://deno.land/x/opine@2.3.3/mod.ts';
+
+const port = Number(Deno.args[0]);
+const pathToIndexHTML = `${Deno.cwd()}/build`;
+const app = opine();
+app.use(serveStatic(pathToIndexHTML));
+
+app.get('/', function (req, res) {
+	console.log(`serving index html from ${pathToIndexHTML}`);
+	res.sendFile(`${pathToIndexHTML}/index.html`);
+});
+
+app.listen(port, () => console.log(`server has started on http://localhost:${port} 🚀`));
