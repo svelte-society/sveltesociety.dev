939f1ecc8b64eee27265f1ad13da2bcb62e74dc0
Michael <michael.spengler@sap.com>
2022-09-24 21:01:27 +0200

    add cultdao in 100 seconds video - I might propose this as a act of revolt

--- docs/_app/immutable/chunks/2-64e2a653.js
+++
@@ -1,6 +0,0 @@
-import { default as e } from '../components/pages/about/_layout.svelte-e6cb6023.js';
-import './index-2fad9c0c.js';
-import './Seo-a81880ba.js';
-import './stores-d20cd678.js';
-import './singletons-7010749a.js';
-export { e as component };
--- docs/_app/immutable/modules/pages/events/_page.ts-ca5ba8ed.js
+++
@@ -1,3 +0,0 @@
-import '../../../chunks/preload-helper-e2690c66.js';
-import { l } from '../../../chunks/_page-c6f0a383.js';
-export { l as load };
