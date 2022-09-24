fdde3f9e8bae4aa29da7ce26d288402a8562d078
Michael <michael.spengler@sap.com>
2022-09-24 21:29:17 +0200

    chase down an error

--- docs/_app/immutable/chunks/7-ae5cff4b.js
+++
@@ -1,8 +0,0 @@
-import { _ as a } from './_page-c6f0a383.js';
-import { default as s } from '../components/pages/events/_page.svelte-7de4e84a.js';
-import './preload-helper-e2690c66.js';
-import './index-2fad9c0c.js';
-import './Seo-a81880ba.js';
-import './stores-d20cd678.js';
-import './singletons-7010749a.js';
-export { s as component, a as shared };
