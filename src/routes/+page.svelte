f26e8887b0f5a03547cb0c5cd75aa9a911bf671b
Michael <michael.spengler@sap.com>
2022-10-04 07:45:15 +0200

    tidy up landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,20 +8,19 @@
 	description="We are a network of cultdao.io fans promoting freedom, fairness, education and love."
 />
 
-<article class="container">
-	<h2 class="text-center">CULT Magazine</h2>
-	<section class="text-center">
-		We foster decentralization to ensure there is no ground for corrupt politicians.
-		<p><br /></p>
-		The CULT Magazine might inspire people to explore and potentially improve architectures of freedom.
-		<p><br /></p>
-		Everyone is invited to contribute via
-		<a href="https://github.com/michael-spengler/cultmagazine" target="_blank">pull request</a>
-		and to be rewarded via
-		<a href="#Revolt2EarnVideo">Revolt 2 Earn</a>.
-		<p><br /></p>
-	</section>
+<h2 class="text-center">CULT Magazine</h2>
+<section class="text-center">
+	We foster decentralization to ensure there is no ground for corrupt politicians.
+	<p><br /></p>
+	The CULT Magazine might inspire people to explore and potentially improve architectures of freedom.
+	<p><br /></p>
+	Everyone is invited to contribute via
+	<a href="https://github.com/michael-spengler/cultmagazine" target="_blank">pull request</a>
+	and to be rewarded via
+	<a href="#Revolt2EarnVideo">Revolt 2 Earn</a>.
+</section>
 
+<div class="iframediv">
 	<iframe
 		class="rumble"
 		width="100%"
@@ -31,23 +30,21 @@
 		allowfullscreen
 		title="Getting Ready"
 	/>
+</div>
 
-	<div class="text-center">
-		<a href="https://cultdao.io/manifesto.pdf">CULT</a> <br /><br /> is the financial hub of a new,
-		free society.
-		<p><br /><br /></p>
-		<a href="https://cultdao.io/rvlt.pdf">Revolt 2 Earn</a> <br /><br /> is the council & support
-		system.
-		<p><br /><br /></p>
-		<p><br /></p>
+<div class="text-center">
+	<a href="https://cultdao.io/manifesto.pdf">CULT</a> <br /><br /> is the financial hub of a new,
+	free society.
+	<p><br /><br /></p>
+	<a href="https://cultdao.io/rvlt.pdf">Revolt 2 Earn</a> <br /><br /> is the council & support
+	system.
 
-		<h3>CULTDAO in 100 Seconds with Michael Saylor</h3>
-		<p><br /></p>
-		Please talk about
-		<a href="https://cultdao.io" target="_blank">CULT</a>
-		after watching this video.
-		<p><br /><br /></p>
+	<h3>CULTDAO in 100 Seconds with Michael Saylor</h3>
+	Please talk about
+	<a href="https://cultdao.io" target="_blank">CULT</a>
+	after watching this video.
 
+	<div class="iframediv">
 		<iframe
 			class="rumble"
 			width="100%"
@@ -57,19 +54,15 @@
 			allowfullscreen
 			title="CULTDAO in 100 seconds with Michael Saylor"
 		/>
-
-		<section id="Revolt2EarnVideo">
-			<p><br /><br /><br /><br /></p>
-
-			<h3>Revolt 2 Earn in 100 Seconds with Michael Saylor</h3>
-			<p><br /></p>
-			Please
-			<a href="https://uniswap.org/" target="_blank"> buy the dip </a>
-			and
-			<a href="https://revolt.cultdao.io/submitProposal" target="_blank"> revolt 2 earn </a>
-			after watching this video.
-			<p><br /><br /></p>
-
+	</div>
+	<section id="Revolt2EarnVideo">
+		<h3>Revolt 2 Earn in 100 Seconds with Michael Saylor</h3>
+		Please
+		<a href="https://uniswap.org/" target="_blank"> buy the dip </a>
+		and
+		<a href="https://revolt.cultdao.io/submitProposal" target="_blank"> revolt 2 earn </a>
+		after watching this video.
+		<div class="iframediv">
 			<iframe
 				class="rumble"
 				width="100%"
@@ -79,49 +72,47 @@
 				allowfullscreen
 				title="CULTDAO in 100 seconds with Michael Saylor"
 			/>
-		</section>
-		<p><br /><br /><br /><br /></p>
-		<h3>Join Us We Rock</h3>
-	</div>
-
-	<section id="communitylinks">
-		<ul>
-			<Link path="https://discord.gg/wearecultdao">
-				<img src="images/discord.svg" alt="" />
-				Discord
-			</Link>
-			<Link path="https://rumble.com/c/c-1902267">
-				<img src="images/youtube.svg" alt="" />
-				YouTube
-			</Link>
-			<Link path="https://twitter.com/MrOmodulus">
-				<img src="images/twitter.svg" alt="" />
-				Twitter
-			</Link>
-		</ul>
-		<ul>
-			<Link path="https://doc.cultdao.io/">
-				<img src="images/newsletter.svg" alt="" />
-				Newsletter
-			</Link>
-			<Link path="https://www.reddit.com/r/cultdao/">
-				<img src="images/reddit.svg" alt="" />
-				Reddit
-			</Link>
-			<Link path="https://www.cultradio.com/">
-				<img src="images/radio.svg" alt="" />
-				Podcast
-			</Link>
-		</ul>
+		</div>
 	</section>
+	<h3>Join Us We Rock</h3>
+</div>
 
-	<div class="text-center">
-		<section id="rumble">
-			<p><br /></p>
-			<h3>We Support rumble.com</h3>
-			<br />
-			Please copy your videos from youtube to rumble. We support rumble via IPFS backups.
-			<p><br /><br /></p>
+<section id="communitylinks">
+	<ul>
+		<Link path="https://discord.gg/wearecultdao">
+			<img src="images/discord.svg" alt="" />
+			Discord
+		</Link>
+		<Link path="https://rumble.com/c/c-1902267">
+			<img src="images/youtube.svg" alt="" />
+			YouTube
+		</Link>
+		<Link path="https://twitter.com/MrOmodulus">
+			<img src="images/twitter.svg" alt="" />
+			Twitter
+		</Link>
+	</ul>
+	<ul>
+		<Link path="https://doc.cultdao.io/">
+			<img src="images/newsletter.svg" alt="" />
+			Newsletter
+		</Link>
+		<Link path="https://www.reddit.com/r/cultdao/">
+			<img src="images/reddit.svg" alt="" />
+			Reddit
+		</Link>
+		<Link path="https://www.cultradio.com/">
+			<img src="images/radio.svg" alt="" />
+			Podcast
+		</Link>
+	</ul>
+</section>
+
+<div class="text-center">
+	<section id="rumble">
+		<h3>We Support rumble.com</h3>
+		Please copy your videos from youtube to rumble. We support rumble via IPFS backups.
+		<div class="iframediv">
 			<iframe
 				class="rumble"
 				width="100%"
@@ -131,37 +122,34 @@
 				allowfullscreen
 				title="CULTDAO in 100 seconds with Michael Saylor"
 			/>
-		</section>
+		</div>
+	</section>
+</div>
+
+<section id="stateofthegame">
+	<div class="text-center">
+		<h3>State of the Game in September 2022</h3>
+		During the upcoming winter, WEF politicians want you hungry, cold and full of fear so that they can
+		offer you things like Central Bank Digital Currencies (CBDCs)... with which they want to expand their
+		power and take away your rights and freedom.<br /><br />
+		<img
+			src="https://raw.githubusercontent.com/cultfamily-on-github/cult-artefacts-collection/main/cult-memes/beat-corrupt-politicians.png"
+			alt="truth seekers"
+			class="centeredImage"
+		/> <br />
+		This is why WEF politicians force young people (aka soldiers) to kill the victims of the other propaganda
+		mutually. <br /><br />
+		This is why WEF politicians want to destroy farmers and food facilities.<br /><br />
+		This is why WEF politicians want to keep you stupid.
+		<p><br /><br /></p>
+		Guess what. Those bitches tried to fuck the wrong guys.<br /><br /><br />
 	</div>
 
-	<section id="stateofthegame">
-		<div class="text-center">
-			<h3>State of the Game in September 2022</h3>
-			<p><br /></p>
+	<div class="text-center">
+		<h3>Time For Some Action</h3>
+		Let's get ready for the party and make a movement.
 
-			During the upcoming winter, WEF politicians want you hungry, cold and full of fear so that
-			they can offer you things like Central Bank Digital Currencies (CBDCs)... with which they want
-			to expand their power and take away your rights and freedom.<br /><br />
-			<img
-				src="https://raw.githubusercontent.com/cultfamily-on-github/cult-artefacts-collection/main/cult-memes/beat-corrupt-politicians.png"
-				alt="truth seekers"
-				class="centeredImage"
-			/> <br />
-			This is why WEF politicians force young people (aka soldiers) to kill the victims of the other
-			propaganda mutually. <br /><br />
-			This is why WEF politicians want to destroy farmers and food facilities.<br /><br />
-			This is why WEF politicians want to keep you stupid.
-			<p><br /><br></p>
-			Guess what. Those bitches tried to fuck the wrong guys.<br /><br /><br />
-		</div>
-
-		<div class="text-center">
-			<h3>Time For Some Action</h3>
-			<p><br /></p>
-			Let's get ready for the party and make a movement.
-
-			<p><br /></p>
-
+		<div class="iframediv">
 			<iframe
 				class="rumble"
 				width="100%"
@@ -171,54 +159,48 @@
 				allowfullscreen
 				title="The A-Team Supports Rumble"
 			/>
+		</div>
 
-			<p><br /><br /></p>
+		<ol>
+			<li>Share this page with your friends</li>
+			<br />
+			<li>
+				Join <a href="https://discord.com/invite/wearecultdao" target="_blank"> our community</a>
+				and
+				<a href="https://t.me/cultmagazine_bot" target="_blank"> our telegram bot </a>
+				to educate yourself and others
+			</li>
+			<br />
+			<li>
+				Install <a href="https://metamask.io" target="_blank"> metamask.io </a>
+				or another browserwallet you fully trust
+			</li>
+			<br />
+			<li>
+				Add CULT asset --> custom token in metamask via smart contract address
+				0xf0f9d895aca5c8678f706fb8216fa22957685a13
+			</li>
+			<br />
+			<li>
+				Buy (the dip) CULT on
+				<a href="https://uniswap.org" target="_blank"> uniswap.org </a>
+			</li>
+			<br />
+			<li>Bookmark the fancy page which you are currently on</li>
+			<br />
+			<li>
+				Let us learn
+				<a href="https://www.youtube.com/watch?v=09maaUaRT4M" target="_blank"> from that</a>
+				and heal
+			</li>
+		</ol>
 
-			<ol>
-				<li>Share this page with your friends</li>
-				<br />
-				<li>
-					Join <a href="https://discord.com/invite/wearecultdao" target="_blank">
-						our community</a
-					> and 
-					<a href="https://t.me/cultmagazine_bot" target="_blank">
-						our telegram bot
-					</a>
-					to educate yourself and others
-				</li>
-				<br />
-				<li>
-					Install <a href="https://metamask.io" target="_blank"> metamask.io </a>
-					or another browserwallet you fully trust
-				</li>
-				<br />
-				<li>
-					Add CULT asset --> custom token in metamask via smart contract address
-					0xf0f9d895aca5c8678f706fb8216fa22957685a13
-				</li>
-				<br />
-				<li>
-					Buy (the dip) CULT on
-					<a href="https://uniswap.org" target="_blank"> uniswap.org </a>
-				</li>
-				<br />
-				<li>Bookmark the fancy page which you are currently on</li>
-				<br />
-				<li>
-					Let us learn
-					<a href="https://www.youtube.com/watch?v=09maaUaRT4M" target="_blank"> from that</a>
-					and heal
-				</li>
-			</ol>
+		<h3>We are the A-Team</h3>
 
-			<p><br /><br /></p>
+		CULT is the financial hub of a new, free society.<br /><br />
+		RVLT is the council & support system.<br /><br />
 
-			<h3>We are the A-Team</h3>
-			<p><br /></p>
-
-			CULT is the financial hub of a new, free society.<br /><br />
-			RVLT is the council & support system.<br /><br />
-
+		<div class="iframediv">
 			<iframe
 				class="rumble"
 				width="100%"
@@ -228,12 +210,11 @@
 				allowfullscreen
 				title="The A-Team Supports"
 			/>
-			<p><br /><br /><br /><br /></p>
-
-			The power they took from the people will return to the people.
 		</div>
-	</section>
-</article>
+
+		The power they took from the people will return to the people.
+	</div>
+</section>
 
 <style>
 	.centeredImage {
@@ -241,11 +222,6 @@
 		margin-left: auto;
 		margin-right: auto;
 		width: 50%;
-	}
-	article {
-		max-width: 1024px;
-		display: grid;
-		gap: var(--s-10);
 	}
 
 	section {
@@ -262,11 +238,5 @@
 	}
 	img {
 		width: 128px;
-	}
-
-	@media (min-width: 1280px) {
-		article {
-			gap: var(--s-20);
-		}
 	}
 </style>
