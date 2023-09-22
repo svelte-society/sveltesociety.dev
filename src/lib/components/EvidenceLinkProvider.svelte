f030580438b77b6364c32de5b9b7e448d3e90585
Monique <michael.spengler@fh-hwz.ch>
2023-09-22 11:31:33 +0200

    complete the freedom of speech dapp

--- src/lib/components/EvidenceLinkProvider.svelte
+++ src/lib/components/EvidenceLinkProvider.svelte
@@ -17,7 +17,7 @@
 	let visitorWantsToInvest;
 	let oneHalFinneyInEther = 0.001;
 	let ethAmountToBeInvested;
-	let evidenceLink;
+	let supportLink = "";
 	let freedomFanIDOfVisitor;
 	let freedomFanInfoOfVisitor = {};
 	let notYetApprovedFreedomFans = [];
@@ -27,6 +27,7 @@
 	let numberOfFreedomFans;
 	let votingMode = false;
 	let showApprovedFreedomFansMode = false;
+	let exampleSupportLink = 'https://twitter.com/monique_is_free/status/1703679856200880545';
 
 	async function handleWalletConnected(event) {
 		publicWalletAddressOfVisitor = event.detail.publicWalletAddress;
@@ -44,8 +45,18 @@
 		numberOfFreedomFans = await contract.numberOfFreedomFans();
 		freedomFanIDOfVisitor = await contract.getIDOfFreedomFan(publicWalletAddressOfVisitor);
 
-		freedomFanInfoOfVisitor = await getFreedomFanInfoFromID(freedomFanIDOfVisitor);
-		await collectFreedomFans();
+		if (freedomFanIDOfVisitor > 0) {
+			freedomFanInfoOfVisitor = await getFreedomFanInfoFromID(freedomFanIDOfVisitor);
+		}
+
+		try {
+			await collectFreedomFans();
+		} catch (error) {
+			confirm(error.message);
+			confirm(
+				'Sorry there might be a synchronization delay. Please reload this page after some seconds.'
+			);
+		}
 		readyForFreedom = true;
 	}
 
@@ -55,7 +66,7 @@
 		let array = [];
 		array = rawInfo.toString().split(',');
 		freedomFanInfo.address = array[0];
-		freedomFanInfo.evidenceLink = array[1];
+		freedomFanInfo.supportLink = array[1];
 		freedomFanInfo.appliedOn = array[2];
 		freedomFanInfo.approvedOn = array[3];
 		freedomFanInfo.amountOfReceivedApprovals = array[4];
@@ -66,7 +77,7 @@
 	async function collectFreedomFans() {
 		for (let i = 1; i <= numberOfFreedomFans; i++) {
 			const freedomFan = await getFreedomFanInfoFromID(i);
-			if ((freedomFan.approvedOn = 0)) {
+			if (freedomFan.approvedOn == 0) {
 				notYetApprovedFreedomFans.push(freedomFan);
 			} else {
 				approvedFreedomFans.push(freedomFan);
@@ -75,17 +86,21 @@
 	}
 
 	async function joinFreedomFans() {
-		try {
-			await contract.joinFreedomFans(evidenceLink);
-		} catch (error) {
-			alert(error.message);
+		if (supportLink.length < 4) {
+			confirm(`please provide a proper support link like ${exampleSupportLink}`);
+		} else {
+			try {
+				await contract.joinFreedomFans(supportLink);
+			} catch (error) {
+				alert(error.message);
+			}
+			confirm(
+				`thank you. this page will be reloaded in some seconds to get the latest data from the chain`
+			);
+			setTimeout(() => {
+				window.location.reload();
+			}, 7 * 1000);
 		}
-		confirm(
-			`thank you. this page will be reloaded in some seconds to get the latest data from the chain`
-		);
-		setTimeout(() => {
-			window.location.reload();
-		}, 3 * 1000);
 	}
 
 	async function wantToInvest() {
@@ -101,11 +116,12 @@
 		const options = { value: Number(ethAmountToBeInvested) * 10 ** 18 };
 		try {
 			await contract.buy(options);
+			confirm(
+				'this page will auto reload in some seconds so you can see your then current amount of coins'
+			);
 			setTimeout(() => {
-				confirm(
-					'this page will auto reload in some seconds so you can see your then current amount of coins'
-				);
-			}, 5 * 1000);
+				window.location.reload();
+			}, 7 * 1000);
 		} catch (error) {
 			alert(error.message);
 		}
@@ -117,40 +133,39 @@
 				`you have tried to sell ${amountOfCoinsToBeSold}. You only have ${amountOfCoinsInVisitorsWallet} in this wallet`
 			);
 		} else {
-			confirm(`you are about to sell ${amountOfCoinsToBeSold} Coins.`);
 			try {
 				const allowance = await contract.allowance(
 					publicWalletAddressOfVisitor,
 					smartContractAddressOnChain
 				);
-				if (allowance < amountOfCoinsToBeSold) {
+				if (Number(allowance) < amountOfCoinsToBeSold) {
 					await contract.increaseAllowance(
 						smartContractAddressOnChain,
 						BigInt(Number(amountOfCoinsToBeSold) * 10 ** 18)
 					); // function needs the input in wei
-					confirm(
-						`setting the allowance - please wait some seconds while executing the first of 2 transactions necessary for the sale of ${amountOfCoinsToBeSold} Coins.`
-					);
-					setTimeout(async () => {
-						await contract.sell(BigInt(Number(amountOfCoinsToBeSold) * 10 ** 18)); // function needs the input in wei
-					}, 5 * 1000);
+					confirm(`Setting the allowance first. Please repeat after about 20 seconds.`);
 				} else {
+					alert(amountOfCoinsToBeSold);
 					await contract.sell(BigInt(Number(amountOfCoinsToBeSold) * 10 ** 18)); // function needs the input in wei
 				}
 			} catch (error) {
 				alert(error.message);
 			}
-			confirm(
-				'this page will auto reload in some seconds so you can see your then current amount of coins'
-			);
-			setTimeout(() => {
-				window.location.reload();
-			}, 7 * 1000);
 		}
 	}
 
 	async function approve(addressOfFreedomFanToBeApproved) {
-		await contract.approveFreedomFan(addressOfFreedomFanToBeApproved);
+		try {
+			await contract.approveFreedomFan(addressOfFreedomFanToBeApproved);
+		} catch (error) {
+			confirm(error.message);
+		}
+		confirm(
+			'Thank you. This page will auto reload in some seconds so you can see if the data has already been updated on chain.'
+		);
+		setTimeout(() => {
+			window.location.reload();
+		}, 7 * 1000);
 	}
 
 	function earnRewardsByVoting() {
@@ -162,16 +177,11 @@
 	}
 
 	async function claimCurrentlyAvailableLiquidityBackedMaxRewards() {
-		const currentPotentialMaxRewards = await contract.maxRewardPerFreedomFan(
-			publicWalletAddressOfVisitor
-		);
-		const currentBalanceOfCoinsInSmartContract = await contract.balanceOf(
-			smartContractAddressOnChain
-		);
+		const currentlyAvailableLiquidityBackedMaxRewards = await contract.getCurrentlyAvailableLiquidityBackedMaxRewards();
 		confirm(
-			`currentPotentialMaxRewards: ${currentPotentialMaxRewards}``currentBalanceOfCoinsInSmartContract: ${currentBalanceOfCoinsInSmartContract}`
+			`currentlyAvailableLiquidityBackedMaxRewards: ${currentlyAvailableLiquidityBackedMaxRewards}`
 		);
-		if (currentPotentialMaxRewards > 0 && currentBalanceOfCoinsInSmartContract > 0) {
+		if (currentlyAvailableLiquidityBackedMaxRewards > 0) {
 			try {
 				await contract.claimCurrentlyAvailableLiquidityBackedMaxRewards();
 			} catch (error) {
@@ -180,154 +190,176 @@
 			confirm('this page will auto reload in some seconds so you can see how much coins you have');
 			setTimeout(() => {
 				window.location.reload();
-			}, 5 * 1000);
+			}, 7 * 1000);
 		} else {
 			confirm(`this transaction would not be successful at the moment`);
-			confirm(`currentPotentialMaxRewards: ${currentPotentialMaxRewards}`);
-			confirm(`currentBalanceOfCoinsInSmartContract: ${currentBalanceOfCoinsInSmartContract}`);
 		}
 	}
 </script>
 
-<div class="break">
-	The
+<h3>General Info</h3>
+<p><br /></p>
+The
+<a
+	class="break"
+	href="https://github.com/monique-baumann/cultmagazine/blob/staging/smart-contracts/free-julian-assange.sol"
+	target="_blank">smart contract</a
+>
+is deployed on <br />
+<!-- <a href="https://polygon.technology/polygon-zkevm" target="_blank">Polygon zkEVM</a>  -->
+<a href="https://chainlist.org/chain/1101" target="_blank">{targetChainName}</a>.
+<p><br /></p>
+It has a fixed supply of<br /> 24.000.000 Julians.
+<p><br /></p>
+It can be found via the address<br />
+<a
+	class="break"
+	href="https://zkevm.polygonscan.com/token/{smartContractAddressOnChain}"
+	target="_blank">{smartContractAddressOnChain}</a
+>.
+
+<p><br /></p>
+You get a free drink at some Freedom of Speech Parties if you prove you have at least 24
+<a href="https://zkevm.polygonscan.com/token/{smartContractAddressOnChain}" target="_blank"
+	>Julians</a
+>.
+<p><br /></p>
+
+<Metamask {targetChainId} {targetChainName} on:walletConnected={handleWalletConnected} />
+{#if publicWalletAddressOfVisitor != undefined && targetChainId == connectedToChainId}
+	<p><br /> <br /></p>
+	You are connected with wallet:
+	<p><br /></p>
 	<a
-		href="https://github.com/monique-baumann/cultmagazine/blob/staging/smart-contracts/free-julian-assange.sol"
-		target="_blank">smart contract</a
+		class="break"
+		href="https://zkevm.polygonscan.com/address/{publicWalletAddressOfVisitor}"
+		target="_blank">{publicWalletAddressOfVisitor}</a
 	>
-	is deployed on <br />
-	<!-- <a href="https://polygon.technology/polygon-zkevm" target="_blank">Polygon zkEVM</a>  -->
-	<a href="https://chainlist.org/chain/1101" target="_blank">{targetChainName}</a>
-	with the address <br />
-	<a href="https://zkevm.polygonscan.com/token/{smartContractAddressOnChain}" target="_blank"
-		>{smartContractAddressOnChain}</a
-	>.
 
-	<Metamask {targetChainId} {targetChainName} on:walletConnected={handleWalletConnected} />
-	{#if publicWalletAddressOfVisitor != undefined && targetChainId == connectedToChainId}
-		<p><br /> <br /></p>
-		You are connected with wallet:
+	<p />
+
+	{#if readyForFreedom}
 		<p><br /></p>
-		<a href="https://zkevm.polygonscan.com/address/{publicWalletAddressOfVisitor}" target="_blank"
-			>{publicWalletAddressOfVisitor}</a
-		>
-
+		Within the smart contract itself there are currently
 		<p />
+		{amountOfCoinsInSmartContractItself} Julians.
+		<p><br /></p>
+		Your currently connected wallet has
+		<p />
+		{amountOfCoinsInVisitorsWallet} Julians.
+		<p><br /></p>
+		<p><br /></p>
+		{#if freedomFanIDOfVisitor < 1}
+			<input
+				class="myInputField"
+				type="text"
+				bind:value={supportLink}
+				placeholder="Please provide your support link here"
+			/>
+			<p><br /></p>
 
-		{#if readyForFreedom}
+			<button on:click={joinFreedomFans}> Join Freedom Fans </button>
+		{:else if freedomFanInfoOfVisitor.approvedOn > 0}
+			<p><br /><br /></p>
+			<button on:click={earnRewardsByVoting}> Earn Rewards By Voting </button>
 			<p><br /></p>
-			Within the smart contract itself there are currently
-			<p />
-			{amountOfCoinsInSmartContractItself} Julians.
+
+			{#if votingMode}
+				{#if notYetApprovedFreedomFans.length == 0}
+					There are currently no new prospects. Please talk about CULT.
+				{:else}
+					<h2>Prospects To Be Approved</h2>
+					{#each notYetApprovedFreedomFans as freedomFan}
+						<a
+							class="break"
+							href="https://zkevm.polygonscan.com/address/{freedomFan.address}"
+							target="_blank">{freedomFan.address}</a
+						>
+						<p><br /></p>
+
+						<a class="break" href={freedomFan.supportLink} target="_blank"
+							>{freedomFan.supportLink}</a
+						>
+						<p><br /></p>
+						<button on:click={() => approve(freedomFan.address)}> Approve This Freedom Fan</button>
+						<hr />
+						<p><br /></p>
+						<p><br /></p>
+					{/each}
+				{/if}
+			{/if}
 			<p><br /></p>
-			Your currently connected wallet has
-			<p />
-			{amountOfCoinsInVisitorsWallet} Julians.
+			<button on:click={claimCurrentlyAvailableLiquidityBackedMaxRewards}>
+				Claim Currently Available Rewards
+			</button>
 			<p><br /></p>
 			<p><br /></p>
-			{#if freedomFanIDOfVisitor == undefined}
+			{#if visitorWantsToInvest}
 				<input
 					class="myInputField"
-					type="text"
-					bind:value={evidenceLink}
-					placeholder="Please provide your evidence link here"
+					type="number"
+					bind:value={ethAmountToBeInvested}
+					placeholder="Invest max 1 Finney = 0.001"
+					min="0"
+					max={oneHalFinneyInEther}
+					step="0.00001"
 				/>
 				<p><br /></p>
-
-				{#if evidenceLink != undefined}
-					<button on:click={joinFreedomFans}> Join Freedom Fans </button>
-				{/if}
+				<button on:click={buyCoinsForEther}> Invest Now </button>
+				<p><br /></p>
 			{:else}
-				<p><br /><br /></p>
-				<button on:click={earnRewardsByVoting}> Earn Rewards By Voting </button>
-				<p><br /></p>
-
-				{#if votingMode}
-					{#if notYetApprovedFreedomFans.length == 0}
-						There are currently no new prospects. Please talk about CULT.
-					{:else}
-						<h2>Prospects To Be Approved</h2>
-						{#each notYetApprovedFreedomFans as freedomFan}
-							<a href="https://zkevm.polygonscan.com/address/{freedomFan.address}" target="_blank"
-								>{freedomFan.address}</a
-							>
-							<p><br></p>
-
-							<a href={freedomFan.evidenceLink} target="_blank">{freedomFan.evidenceLink}</a>
-							<p><br></p>
-							<hr>
-							<p><br /></p>
-							<button on:click={approve(freedomFan.address)}> Approve </button>
-							<p><br /></p>
-						{/each}
-					{/if}
-				{/if}
-				<p><br /></p>
-				<button on:click={claimCurrentlyAvailableLiquidityBackedMaxRewards}>
-					Claim Currently Available Rewards
-				</button>
-				<p><br /></p>
-				<p><br /></p>
-				{#if visitorWantsToInvest}
-					<input
-						class="myInputField"
-						type="number"
-						bind:value={ethAmountToBeInvested}
-						placeholder="Invest max 1 Finney"
-						min="0"
-						max={oneHalFinneyInEther}
-						step="0.00001"
-					/>
-					<p><br /></p>
-					<button on:click={buyCoinsForEther}> Invest Now </button>
-					<p><br /></p>
-				{:else}
-					<button on:click={wantToInvest}> Invest up to 1 Finney </button>
-				{/if}
-				<p><br /></p>
-				<p><br /></p>
-				{#if visitorWantsToSellCoins}
-					<input
-						class="myInputField"
-						type="number"
-						bind:value={amountOfCoinsToBeSold}
-						placeholder="How much to sell?"
-						max={amountOfCoinsInVisitorsWallet}
-					/>
-					<p><br /></p>
-					<button on:click={sellCoins}> Sell Now </button>
-					<p><br /></p>
-				{:else}
-					<button on:click={wantToSellCoins}> Sell Coins </button>
-				{/if}
+				<button on:click={wantToInvest}> Invest up to 1 Finney </button>
 			{/if}
-
-			<p><br /><br /></p>
-			<button on:click={showApprovedFreedomFans}> Show Approved Freedom Fans </button>
 			<p><br /></p>
-
-			{#if showApprovedFreedomFansMode}
-			<p><br></p>
-				<h2>Approved Freedom Fans</h2>
-				{#each approvedFreedomFans as freedomFan}
-					<a href="https://zkevm.polygonscan.com/address/{freedomFan.address}" target="_blank"
-						>{freedomFan.address}</a
-					>
-					<p><br></p>
-					<a href={freedomFan.evidenceLink} target="_blank">{freedomFan.evidenceLink}</a>
-
-					<p><br></p>
-					<hr>
-					<p><br /></p>
-
-				{/each}
+			<p><br /></p>
+			{#if visitorWantsToSellCoins}
+				<input
+					class="myInputField"
+					type="number"
+					bind:value={amountOfCoinsToBeSold}
+					placeholder="How much to sell?"
+					max={amountOfCoinsInVisitorsWallet}
+				/>
+				<p><br /></p>
+				<button on:click={sellCoins}> Sell Now </button>
+				<p><br /></p>
+			{:else}
+				<button on:click={wantToSellCoins}> Sell Coins </button>
 			{/if}
+		{/if}
 
-			<p><br /><br /></p>
-			The evidence link you have provided is:
+		<p><br /><br /></p>
+		<button on:click={showApprovedFreedomFans}> Show Approved Freedom Fans </button>
+		<p><br /></p>
+
+		{#if showApprovedFreedomFansMode}
 			<p><br /></p>
-			<a href={freedomFanInfoOfVisitor.evidenceLink} target="_blank"
-				>{freedomFanInfoOfVisitor.evidenceLink}</a
+			<h3>Approved Freedom Fans</h3>
+			<p><br /></p>
+			{#each approvedFreedomFans as freedomFan}
+				<a class="break" href={freedomFan.supportLink} target="_blank">{freedomFan.supportLink}</a>
+				<p><br /></p>
+				<a
+					class="break"
+					href="https://zkevm.polygonscan.com/address/{freedomFan.address}"
+					target="_blank">{freedomFan.address}</a
+				>
+
+				<p><br /></p>
+				<hr />
+				<p><br /></p>
+			{/each}
+		{/if}
+
+		{#if freedomFanIDOfVisitor > 0}
+			<p><br /></p>
+			<h3>Your Master Data</h3>
+
+			<p><br /></p>
+			The support link you have provided is:
+			<p><br /></p>
+			<a class="break" href={freedomFanInfoOfVisitor.supportLink} target="_blank"
+				>{freedomFanInfoOfVisitor.supportLink}</a
 			>
 			<p><br /></p>
 			You have applied on<br />
@@ -340,7 +372,7 @@
 			<p><br /></p>
 		{/if}
 	{/if}
-</div>
+{/if}
 
 <style>
 	.myInputField {
