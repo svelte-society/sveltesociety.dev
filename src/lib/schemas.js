9fff772ded99eb57079aa02cdb85cda662d868ad
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-11-06 09:08:27 +1100

    Add zod schemas for CI validation

---
+++ src/lib/schemas.js
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+
+export const componentsSchema = z.array(
+	z.object({
+		title: z.string(),
+		url: z.string().optional(),
+		repository: z.string(),
+		description: z.string(),
+		npm: z.string().optional(),
+		category: z.string(),
+		tags: z.array(z.string()).optional()
+	})
+);
+
+export const templatesSchema = z.array(
+	z.object({
+		title: z.string(),
+		repository: z.string(),
+		description: z.string(),
+		category: z.string(),
+		tags: z.array(z.string()).optional()
+	})
+);
+
+export const toolsSchema = z.array(
+	z.object({
+		title: z.string(),
+		repository: z.string(),
+		description: z.string(),
+		category: z.string(),
+		tags: z.array(z.string()).optional()
+	})
+);
