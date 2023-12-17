2379a37f9a99af66a2e9cf4786ed7eaa9747d71c
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-17 16:29:21 +1100

    Restructure npm.json output

--- scripts/updateNpm.js
+++ scripts/updateNpm.js
@@ -14,17 +14,7 @@ const data = [...componentsSchema.parse(components), ...toolsSchema.parse(tools)
 const npm = await Promise.all(
 	data.map((pkg) => processPackage(pkg).catch((error) => console.log(error.message)))
 ).then((values) => {
-	return values.reduce(
-		(result, value) => {
-			if (value.name) {
-				result.versions[value.name] = value.version;
-				result.dates[value.name] = value.date;
-				result.support[value.name] = value.support;
-			}
-			return result;
-		},
-		{ versions: {}, dates: {}, support: {} }
-	);
+	return values.reduce((result, value) => Object.assign(result, value), {});
 });
 
 writeFileSync('src/lib/npm.json', JSON.stringify(npm));
@@ -36,5 +26,5 @@ async function processPackage(pkg) {
 	const version = data.version;
 	const date = data.time[version];
 	const support = data.peerDependencies?.svelte ? data.peerDependencies.svelte : 'Unknown';
-	return { name: pkg.npm, version: version, date: date, support: support };
+	return { [pkg.npm]: { version: version, date: date, support: support } };
 }
