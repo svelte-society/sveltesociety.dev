26eabb182080abd231e216dbf1fd345cac44e600
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-18 14:03:18 +1100

    Simplify injectData

--- src/lib/utils/injectData.ts
+++ src/lib/utils/injectData.ts
@@ -5,44 +5,28 @@ import publint from '$lib/data/publint.json';
 import type { z } from 'zod';
 import type { componentsSchema } from '$lib/schemas';
 
-export const injectGithubData = (input: z.infer<typeof componentsSchema>) => {
-	const output = [];
-	for (const item of input) {
-		const index = Object.keys(github).find((key) => item.repository.toLowerCase().includes(key));
-		const extra = github[index] ?? {};
-		output.push({ ...item, ...extra });
-	}
-	return output;
-};
-
-export const injectGitlabData = (input: z.infer<typeof componentsSchema>) => {
-	const output = [];
-	for (const item of input) {
-		const index = Object.keys(gitlab).find((key) => item.repository.toLowerCase().includes(key));
-		const extra = gitlab[index] ?? {};
-		output.push({ ...item, ...extra });
-	}
-	return output;
-};
-
-export const injectNpmData = (input: z.infer<typeof componentsSchema>) => {
-	const output = [];
-	for (const item of input) {
-		const extra = npm[item.npm] ?? {};
-		output.push({ ...item, ...extra });
-	}
-	return output;
-};
-
-export const injectPublintData = (input: z.infer<typeof componentsSchema>) => {
-	const output = [];
-	for (const item of input) {
-		const extra = publint[item.npm] ?? false;
-		output.push({ ...item, publint: extra });
-	}
-	return output;
-};
-
 export const injectData = (input: z.infer<typeof componentsSchema>) => {
-	return injectGithubData(injectGitlabData(injectNpmData(injectPublintData(input))));
+	const output = [];
+	for (const item of input) {
+		// Github
+		const githubIndex = Object.keys(github).find((key) =>
+			item.repository.toLowerCase().includes(key)
+		);
+		const githubExtra = github[githubIndex] ?? {};
+
+		// Gitlab
+		const gitlabIndex = Object.keys(gitlab).find((key) =>
+			item.repository.toLowerCase().includes(key)
+		);
+		const gitlabExtra = gitlab[gitlabIndex] ?? {};
+
+		// NPM
+		const npmExtra = npm[item.npm] ?? {};
+
+		// Publint
+		const publintExtra = publint[item.npm] ? { publint: true } : { publint: false };
+
+		output.push({ ...item, ...githubExtra, ...gitlabExtra, ...npmExtra, ...publintExtra });
+	}
+	return output;
 };
--- src/routes/tools/+page.svelte
+++ src/routes/tools/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import tools from '../tools/tools.json';
 	import SearchableJson from '$lib/SearchableJson.svelte';
-	import { injectNpmData } from '$utils/injectData';
-	import { injectStars } from '$utils/stars';
+	import { injectData } from '$utils/injectData';
 </script>
 
 <SearchableJson
-	data={injectNpmData(injectStars(tools))}
+	data={injectData(tools)}
 	displayTitle="Tools"
 	displayTitleSingular="tool"
 	submittingType="tool"
