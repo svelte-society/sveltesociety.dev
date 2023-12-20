740cb85f61d4a81a04679d8e8d0a68e4bb738879
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-21 09:06:49 +1100

    Max length on title and description

--- src/lib/schemas.js
+++ src/lib/schemas.js
@@ -3,11 +3,11 @@ import { packageNameRegex } from 'package-name-regex';
 
 export const packagesSchema = z.array(
 	z.object({
-		title: z.string(),
+		title: z.string().max(50),
 		npm: z.string().regex(packageNameRegex),
 		url: z.string().url().optional(),
 		repository: z.string().url(),
-		description: z.string(),
+		description: z.string().max(250),
 		category: z.enum([
 			'Bundler Plugins',
 			'CSS and Layout',
@@ -36,10 +36,10 @@ export const packagesSchema = z.array(
 
 export const templatesSchema = z.array(
 	z.object({
-		title: z.string(),
+		title: z.string().max(50),
 		url: z.string().url().optional(),
 		repository: z.string().url(),
-		description: z.string(),
+		description: z.string().max(250),
 		category: z.enum(['Svelte Add', 'SvelteKit', 'Svelte']),
 		tags: z.array(z.string()).optional()
 	})
