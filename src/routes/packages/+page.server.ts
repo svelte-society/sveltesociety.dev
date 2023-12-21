1ef88296d0c12e17b37f81a94fe58927977017ae
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-21 17:44:28 +1100

    Require all tags to be present

--- src/routes/packages/+page.server.ts
+++ src/routes/packages/+page.server.ts
@@ -2,25 +2,19 @@ import { packagesSchema } from '$lib/schemas.js';
 import { injectData } from '$utils/injectData';
 import packages from './packages.json';
 
-export const load = async ({url}) => {
-    const data = injectData(packagesSchema.parse(packages))
+export const load = async ({ url }) => {
+	const data = injectData(packagesSchema.parse(packages));
 
 	const tags = url.searchParams.get('tags').split(',');
-    console.log(tags);
+	console.log(tags);
 
-    if (!tags) {
-        return { packages: data }
-    }
+	if (!tags) {
+		return { packages: data };
+	}
 
-    const filteredData = data.filter((entry) => {
-        if (entry.tags) {
-            for (const tag of tags) {
-                if (entry.tags.includes(tag)) {
-                    return true
-                }
-            }
-        }
-    })
+	const filteredData = data.filter((entry) => {
+		return tags.every((val) => entry.tags.includes(val));
+	});
 
-    return { packages: filteredData }
+	return { packages: filteredData };
 };
