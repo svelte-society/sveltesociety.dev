64952ea6b782a17b7f616e86ffad850c08e53c64
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-24 08:31:10 +1100

    feat: Sort packages by last update

--- src/lib/SearchableJson.svelte
+++ src/lib/SearchableJson.svelte
@@ -32,7 +32,8 @@
 				bind:query={searchValue}
 				sortableFields={[
 					{ identifier: 'stars', title: 'Stars', ascending: false },
-					{ identifier: 'title', title: 'Name', ascending: true }
+					{ identifier: 'title', title: 'Name', ascending: true },
+					{ identifier: 'date', title: 'Date', ascending: false }
 				]}
 				searchableFields={['title', 'description']}
 				facetsConfig={[
