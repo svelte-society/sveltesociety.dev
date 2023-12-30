536af745106e6a08a801864c0e8ba7631770b01b
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-30 18:19:36 +1100

    Add category to tags

--- src/lib/schemas.js
+++ src/lib/schemas.js
@@ -1,6 +1,66 @@
 import { z } from 'zod';
 import { packageNameRegex } from 'package-name-regex';
 
+export const PACKAGES_TAGS = /** @type {const} */ ([
+	'animations',
+	'async-data',
+	'async-loading',
+	'audio',
+	'auth',
+	'bundler-plugins',
+	'charts',
+	'cli-tools',
+	'components-and-libraries',
+	'css-and-layout',
+	'data-visualisation',
+	'debugging',
+	'design-pattern',
+	'design-system',
+	'developer-experience',
+	'development-and-documentation',
+	'display-components',
+	'fonts-and-icons',
+	'forms-and-user-input',
+	'forms-and-validation',
+	'graphql',
+	'icons',
+	'images',
+	'in-page-navigation',
+	'inputs-and-widgets',
+	'integration',
+	'integrations',
+	'interactions',
+	'internationalization',
+	'internationalization',
+	'intersection-observer',
+	'jsx',
+	'layout-and-structure',
+	'linting-and-formatting',
+	'maps',
+	'markdown',
+	'modals',
+	'multimedia',
+	'native',
+	'network-events',
+	'notifications',
+	'offline-and-online-detection',
+	'preprocessors',
+	'routers',
+	'routers',
+	'ssr',
+	'stores',
+	'stores-and-state',
+	'sveltekit-adapters',
+	'testing',
+	'testing',
+	'third-party-services',
+	'time-and-date',
+	'typescript',
+	'user-interaction',
+	'video',
+	'viewport'
+]);
+
 export const packagesSchema = z.array(
 	z.object({
 		title: z.string().max(50),
@@ -8,73 +68,34 @@ export const packagesSchema = z.array(
 		url: z.string().url().optional(),
 		repository: z.string().url(),
 		description: z.string().max(250),
-		category: z.enum([
-			'bundler-plugins',
-			'css-and-layout',
-			'data-visualisation',
-			'debugging',
-			'design-pattern',
-			'design-system',
-			'developer-experience',
-			'display-components',
-			'forms-and-user-input',
-			'icons',
-			'integration',
-			'internationalization',
-			'linting-and-formatting',
-			'multimedia',
-			'preprocessors',
-			'routers',
-			'stores',
-			'sveltekit-adapters',
-			'testing',
-			'user-interaction'
-		]),
-		tags: z
-			.array(
-				z.enum([
-					'animations',
-					'async-data',
-					'async-loading',
-					'audio',
-					'auth',
-					'charts',
-					'cli-tools',
-					'components-and-libraries',
-					'development-and-documentation',
-					'fonts-and-icons',
-					'forms-and-validation',
-					'graphql',
-					'images',
-					'in-page-navigation',
-					'inputs-and-widgets',
-					'integrations',
-					'interactions',
-					'internationalization',
-					'intersection-observer',
-					'jsx',
-					'layout-and-structure',
-					'maps',
-					'markdown',
-					'modals',
-					'native',
-					'network-events',
-					'notifications',
-					'offline-and-online-detection',
-					'routers',
-					'ssr',
-					'stores-and-state',
-					'testing',
-					'third-party-services',
-					'time-and-date',
-					'typescript',
-					'video',
-					'viewport'
-				])
-			)
-			.max(5)
+		tags: z.array(z.enum(PACKAGES_TAGS)).max(6)
 	})
 );
+
+export const TEMPLATES_TAGS = /** @type {const} */ ([
+	'blog',
+	'code-splitting',
+	'component-sets',
+	'components-and-libraries',
+	'database',
+	'electron',
+	'integrations',
+	'lazy-loading',
+	'markdown',
+	'mdsvex',
+	'mobile',
+	'preprocessors',
+	'seo',
+	'ssr',
+	'stores-and-state',
+	'storybook',
+	'svelte',
+	'svelte-add',
+	'sveltekit',
+	'testing',
+	'typescript',
+	'webpack'
+]);
 
 export const templatesSchema = z.array(
 	z.object({
@@ -82,31 +103,6 @@ export const templatesSchema = z.array(
 		url: z.string().url().optional(),
 		repository: z.string().url(),
 		description: z.string().max(250),
-		category: z.enum(['svelte-add', 'sveltekit', 'svelte']),
-		tags: z
-			.array(
-				z.enum([
-					'blog',
-					'code-splitting',
-					'component-sets',
-					'components-and-libraries',
-					'database',
-					'electron',
-					'integrations',
-					'lazy-loading',
-					'markdown',
-					'mdsvex',
-					'mobile',
-					'preprocessors',
-					'seo',
-					'ssr',
-					'stores-and-state',
-					'storybook',
-					'testing',
-					'typescript',
-					'webpack'
-				])
-			)
-			.max(5)
+		tags: z.array(z.enum(TEMPLATES_TAGS)).max(6)
 	})
 );
