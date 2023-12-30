af1655f06720c022ebe8cb44093a94547e878b41
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-30 18:07:49 +1100

    Convert tags to kebab-case

--- src/lib/schemas.js
+++ src/lib/schemas.js
@@ -9,28 +9,70 @@ export const packagesSchema = z.array(
 		repository: z.string().url(),
 		description: z.string().max(250),
 		category: z.enum([
-			'Bundler Plugins',
-			'CSS and Layout',
-			'Data Visualisation',
-			'Debugging',
-			'Design Pattern',
-			'Design System',
-			'Developer Experience',
-			'Display Components',
-			'Forms & User Input',
-			'Icons',
-			'Integration',
-			'Internationalization',
-			'Linting and Formatting',
-			'Multimedia',
-			'Preprocessors',
-			'Routers',
-			'Stores',
-			'SvelteKit Adapters',
-			'Testing',
-			'User Interaction'
+			'bundler-plugins',
+			'css-and-layout',
+			'data-visualisation',
+			'debugging',
+			'design-pattern',
+			'design-system',
+			'developer-experience',
+			'display-components',
+			'forms-and-user-input',
+			'icons',
+			'integration',
+			'internationalization',
+			'linting-and-formatting',
+			'multimedia',
+			'preprocessors',
+			'routers',
+			'stores',
+			'sveltekit-adapters',
+			'testing',
+			'user-interaction'
 		]),
-		tags: z.array(z.string()).max(5)
+		tags: z
+			.array(
+				z.enum([
+					'animations',
+					'async-data',
+					'async-loading',
+					'audio',
+					'auth',
+					'charts',
+					'cli-tools',
+					'components-and-libraries',
+					'development-and-documentation',
+					'fonts-and-icons',
+					'forms-and-validation',
+					'graphql',
+					'images',
+					'in-page-navigation',
+					'inputs-and-widgets',
+					'integrations',
+					'interactions',
+					'internationalization',
+					'intersection-observer',
+					'jsx',
+					'layout-and-structure',
+					'maps',
+					'markdown',
+					'modals',
+					'native',
+					'network-events',
+					'notifications',
+					'offline-and-online-detection',
+					'routers',
+					'ssr',
+					'stores-and-state',
+					'testing',
+					'third-party-services',
+					'time-and-date',
+					'typescript',
+					'video',
+					'viewport'
+				])
+			)
+			.max(5)
 	})
 );
 
@@ -40,7 +82,31 @@ export const templatesSchema = z.array(
 		url: z.string().url().optional(),
 		repository: z.string().url(),
 		description: z.string().max(250),
-		category: z.enum(['Svelte Add', 'SvelteKit', 'Svelte']),
-		tags: z.array(z.string()).max(5)
+		category: z.enum(['svelte-add', 'sveltekit', 'svelte']),
+		tags: z
+			.array(
+				z.enum([
+					'blog',
+					'code-splitting',
+					'component-sets',
+					'components-and-libraries',
+					'database',
+					'electron',
+					'integrations',
+					'lazy-loading',
+					'markdown',
+					'mdsvex',
+					'mobile',
+					'preprocessors',
+					'seo',
+					'ssr',
+					'stores-and-state',
+					'storybook',
+					'testing',
+					'typescript',
+					'webpack'
+				])
+			)
+			.max(5)
 	})
 );
