2f596b32f9db17f8387a8b2a6905510d5200b4dd
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-07-07 16:23:41 +0200

    add crude content card component

--- sites/www/src/routes/(app)/+page.svelte
+++ sites/www/src/routes/(app)/+page.svelte
@@ -1,1 +1,22 @@
-Home page!
+<script lang="ts">
+	import ContentCard from '$lib/ui/ContentCard.svelte';
+
+	let tags = [
+		{ id: 'svelte5', name: 'Svelte5' },
+		{ id: 'runes', name: 'Runes' }
+	];
+</script>
+
+<ContentCard
+	title="Working with Actions in Svelte5"
+	description="SvelteKit is a great tool for building modern web applications..."
+	type="Recipe"
+	author="John Doe"
+	time="2 days ago"
+	views="11114"
+	likes="10"
+	{tags}
+	slug="working-with-actions-in-svelte5"
+>
+	Placeholder children content</ContentCard
+>
