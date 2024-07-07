2b773ddd8e3ee6a894d2d758d234026e54a33b0c
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-07-07 15:36:32 +0200

    remove footer

--- sites/www/src/routes/(app)/+layout.svelte
+++ sites/www/src/routes/(app)/+layout.svelte
@@ -95,10 +95,4 @@
 
 		<RightSidebar />
 	</div>
-
-	<footer class="mt-auto bg-gray-800 p-4 text-white">
-		<div class="container mx-auto text-center">
-			<p>&copy; {new Date().getFullYear()} My SvelteKit App. All rights reserved.</p>
-		</div>
-	</footer>
 </div>
