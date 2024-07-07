db6a09c38c3c05e8884a6e3050feb4a14c3e55ba
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-07-07 16:23:27 +0200

    move tags components to ui folder

--- sites/www/src/lib/ui/Tags.svelte
+++ sites/www/src/lib/ui/Tags.svelte
@@ -8,11 +8,8 @@
 	let { tags }: { tags: Tag[] } = $props();
 </script>
 
-<div>
-	<h3 class="text-md mb-2 font-semibold">Popular tags</h3>
-	<div class="mb-4 flex flex-wrap gap-2">
-		{#each tags as tag}
-			<Tag {tag} />
-		{/each}
-	</div>
+<div class="flex flex-wrap gap-2">
+	{#each tags as tag}
+		<Tag {tag} />
+	{/each}
 </div>
