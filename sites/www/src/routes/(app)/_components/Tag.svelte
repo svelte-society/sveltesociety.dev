76a7f77e3f4d1f4ed5c2fd2d6966f1eaadbb3051
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-07-07 14:56:36 +0200

    fix type issue

--- sites/www/src/routes/(app)/_components/Tag.svelte
+++ sites/www/src/routes/(app)/_components/Tag.svelte
@@ -3,7 +3,7 @@
 		id: string;
 		name: string;
 	};
-	let { tag }: { tags: Tag } = $props();
+	let { tag }: { tag: Tag } = $props();
 </script>
 
 <a href="/tag/{tag.id}" class="rounded bg-orange-100 px-1 py-0.5 text-xs text-orange-600">
