9516e682c4965689949a127001c6eb83080686c5
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-07-21 14:18:08 +0200

    implement markdown editor using milkdown package

--- sites/www/src/lib/ui/MarkdownEditor.svelte
+++ sites/www/src/lib/ui/MarkdownEditor.svelte
@@ -1,39 +1,53 @@
 <script lang="ts">
-	import { Carta, MarkdownEditor } from 'carta-md';
-	import 'carta-md/default.css';
+	import { insert } from '@milkdown/utils';
+	import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core';
+	import { commonmark } from '@milkdown/preset-commonmark';
+	import { clipboard } from '@milkdown/plugin-clipboard';
+	import { emoji } from '@milkdown/plugin-emoji';
+	import { history } from '@milkdown/plugin-history';
+	import { listener, listenerCtx } from '@milkdown/plugin-listener';
+	import { upload } from '@milkdown/plugin-upload';
+	import { nord } from '@milkdown/theme-nord';
+	import '@milkdown-lab/plugin-menu/style.css';
 
-	import { attachment } from '@cartamd/plugin-attachment';
-	import { emoji } from '@cartamd/plugin-emoji';
-	import { code } from '@cartamd/plugin-code';
-	import { video } from 'carta-plugin-video';
-	import { insdel } from 'carta-plugin-ins-del';
+	let editor: Editor;
+	let editorDiv: HTMLElement;
 
-	const carta = new Carta({
-		sanitizer: false,
-		extensions: [
-			attachment({
-				async upload() {
-					return 'some-url-from-server.xyz';
-				}
-			}),
-			emoji(),
-			code(),
-			video(),
-			insdel()
-		]
+	let { markdown = 'Test', value = $bindable(), name = '' } = $props();
+
+	$effect(async () => {
+		// const { menu, menuDefaultConfig } = await import('@milkdown-lab/plugin-menu');
+
+		editor = await Editor.make()
+			// .config(menuDefaultConfig)
+			.config((ctx) => {
+				ctx.set(rootCtx, editorDiv);
+			})
+			.config((ctx) => {
+				ctx.get(listenerCtx).markdownUpdated((ctx, markdown, prevMarkdown) => {
+					value = markdown;
+				});
+			})
+			.config(nord)
+			.use(commonmark)
+			.use(clipboard)
+			.use(emoji)
+			.use(history)
+			.use(upload)
+			.use(listener)
+			// .use(menu)
+			.create();
+
+		editor.action(insert(markdown));
+
+		return async () => {
+			await editor.destroy();
+		};
 	});
-
-	let value = '';
 </script>
 
-{#key value}
-	<MarkdownEditor {carta} {value} />
-{/key}
+<div class="markdown-editor">
+	<div bind:this={editorDiv}></div>
+</div>
 
-<style>
-	/* Set your monospace font (Required to have the editor working correctly!) */
-	:global(.carta-font-code) {
-		font-family: '...', monospace;
-		font-size: 1.1rem;
-	}
-</style>
+<input type="hidden" {value} {name} />
