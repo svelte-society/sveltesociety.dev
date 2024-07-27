f7a1fe0d544463bd1bb8959e0350828c2e4dc6d7
MacFJA <macfja@gmail.com>
2024-07-27 18:53:23 +0200

    Add RubyGem package

--- src/lib/components/ComponentIndex/Card.svelte
+++ src/lib/components/ComponentIndex/Card.svelte
@@ -7,22 +7,35 @@
 	export let title: string;
 	export let description: string;
 	export let stars: string;
-	export let npm = '';
-	export let repository = undefined;
+	export let npm: string | undefined = undefined;
+	export let gem: string | undefined = undefined;
+	export let repository: string | undefined = undefined;
 	export let date = undefined;
 	export let version = undefined;
 
 	let clipboardCopy = false;
 
 	const copy = () => {
-		copyToClipboard(`${packageManagers[$manager]} ${npm}`).then(() => (clipboardCopy = false));
+		copyToClipboard(packageManagerAction($manager, npm, gem)).then(() => (clipboardCopy = false));
 		clipboardCopy = true;
+	};
+
+	const packageManagerAction = ($manager: Array<string>, npm?: string, gem?: string): string => {
+		if (npm) {
+			return `${packageManagers[$manager.find((m) => ['npm', 'pnpm', 'yarn'].includes(m)) ?? 'npm']} ${npm}`;
+		}
+		if (gem) {
+			return `${packageManagers[$manager.find((m) => ['gem', 'bundler'].includes(m)) ?? 'gem']} ${gem}`;
+		}
+		return '';
 	};
 
 	const packageManagers = {
 		npm: 'npm install',
 		pnpm: 'pnpm add',
-		yarn: 'yarn add'
+		yarn: 'yarn add',
+		gem: 'gem install',
+		bundler: 'bundle add'
 	};
 </script>
 
@@ -34,7 +47,7 @@
 			</h3>
 		</div>
 		<div>
-			{#if repository.includes('github')}
+			{#if repository?.includes('github')}
 				<a
 					class="repo box-border flex aspect-square rounded-full border-none"
 					title="Go to the source code"
@@ -43,7 +56,7 @@
 				>
 					<img style="display:inline" src="/images/github_logo.svg" alt="github logo" />
 				</a>
-			{:else if repository.includes('gitlab')}
+			{:else if repository?.includes('gitlab')}
 				<a
 					class="repo box-border flex aspect-square rounded-full border-none"
 					title="Go to the source code"
@@ -52,16 +65,24 @@
 				>
 					<img style="display:inline" src="/images/gitlab_logo.svg" alt="gitlab logo" />
 				</a>
-				<!-- {:else} -->
+			{:else if repository}
+				<a
+					class="repo box-border flex aspect-square rounded-full border-none"
+					title="Go to the source code"
+					target="_blank"
+					href={repository}
+				>
+					🌐
+				</a>
 			{/if}
 		</div>
 	</div>
 
-	{#if npm}
+	{#if npm || gem}
 		<Tag
 			click={() => copy()}
 			variant="copy"
-			title={clipboardCopy ? 'copied!' : `${packageManagers[$manager]} ${npm}`}
+			title={clipboardCopy ? 'copied!' : packageManagerAction($manager, npm, gem)}
 		/>
 	{/if}
 	<p class="flex-grow pb-6">{description}</p>
@@ -89,6 +110,9 @@
 		margin: -4px;
 		background-color: rgba(0, 0, 0, 0);
 		transition: background-color 200ms ease-out;
+		text-decoration: none;
+		font-size: 1rem;
+		line-height: 18px;
 	}
 	.repo:hover {
 		background-color: rgba(0, 0, 0, 0.25);
