8e94d6ee05b951918610091a482355d6a2c6fd1c
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-07-29 21:33:09 +0200

    bump version, start conversion to better-sqlite3 instead of drizzle

--- sites/www/vite.config.ts
+++ sites/www/vite.config.ts
@@ -1,9 +1,8 @@
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [enhancedImages(), sveltekit()],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
