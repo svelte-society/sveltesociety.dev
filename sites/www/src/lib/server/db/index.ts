5924db5bfb3dd1e14a68e1a5fa7cb492d661288d
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-08-01 18:45:47 +0200

    use better-sqlite3

--- sites/www/src/lib/server/db/index.ts
+++ sites/www/src/lib/server/db/index.ts
@@ -1,9 +1,5 @@
-import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from "$env/static/private";
-import { createClient } from "@libsql/client/web";
-import { drizzle } from 'drizzle-orm/libsql'
-import * as schema from './schema'
+import Database from 'better-sqlite3';
 
-export const db = drizzle(createClient({
-  url: TURSO_DATABASE_URL,
-  authToken: TURSO_AUTH_TOKEN,
-}), { schema })
+export const db = new Database('local.db')
+db.pragma('journal_mode = WAL')
+db.pragma('foreign_keys = ON')
