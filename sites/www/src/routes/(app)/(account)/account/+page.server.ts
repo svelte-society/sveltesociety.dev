5d35307dc5413e9671aa8638e5695d8cafdeb29c
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-08-05 19:53:13 +0200

    remove unused server load on account page

--- sites/www/src/routes/(app)/(account)/account/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
-
-export const load = (async ({ locals }) => {
-    return {};
-}) satisfies PageServerLoad;
