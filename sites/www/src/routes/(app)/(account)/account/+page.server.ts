30e164facd081a51bdd6c06a92c4073e04a15b60
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-08-05 19:49:51 +0200

    remove user check for account page (now handled via hooks)

--- sites/www/src/routes/(app)/(account)/account/+page.server.ts
+++ sites/www/src/routes/(app)/(account)/account/+page.server.ts
@@ -2,9 +2,5 @@ import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals }) => {
-    if (!locals.user) {
-        throw redirect(302, '/login');
-    }
-
     return {};
 }) satisfies PageServerLoad;
