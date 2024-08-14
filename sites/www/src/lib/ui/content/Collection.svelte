ceae3729ee9a0512832119cf5c37e5392790d89b
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-08-14 22:00:22 +0200

    Fix padding issue in collection items

--- sites/www/src/lib/ui/content/Collection.svelte
+++ sites/www/src/lib/ui/content/Collection.svelte
@@ -8,7 +8,7 @@
 
 <ul class="flex flex-col gap-1.5">
 	{#each children as child}
-		<li class="border border-slate-200 bg-slate-100 p-1.5">
+		<li class="border border-slate-200 bg-slate-100 px-2.5 py-1.5">
 			<h2 class="text-md font-bold"><a href="/{child.type}/{child.slug}">{child.title}</a></h2>
 			<span class="flex text-xs"
 				><span class="mr-0.5 font-bold capitalize">{child.type}</span> posted by {child.author} • {child.published_at}</span
