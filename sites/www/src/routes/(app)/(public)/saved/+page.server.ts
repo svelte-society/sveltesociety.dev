4e87a4fc149e7eaa335314242d19b1ce7ba155f3
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-08-16 17:08:57 +0200

    Add Children support in Saved page

--- sites/www/src/routes/(app)/(public)/saved/+page.server.ts
+++ sites/www/src/routes/(app)/(public)/saved/+page.server.ts
@@ -1,6 +1,6 @@
 import type { PageServerLoad } from './$types';
 import { error, redirect } from '@sveltejs/kit';
-import { get_user_saved_content, get_tags_for_content } from '$lib/server/db/content';
+import { get_user_saved_content, get_tags_for_content, get_content_by_ids } from '$lib/server/db/content';
 import { get_user_likes_and_saves } from '$lib/server/db/interactions';
 get_user_likes_and_saves
 
@@ -12,11 +12,18 @@ export const load = (async ({ url, locals }) => {
 
         const tags = get_tags_for_content(content.map(c => c.id))
         let content_with_tags = content.map((c, i) => ({ ...c, tags: tags[i] || [] }))
+        const all_children = [...new Set(content.reduce((acc, c) => [...acc, ...JSON.parse(c.children ?? '[]')], []))]
+        const children = get_content_by_ids(all_children)
+
+        let content_with_tags_and_children = content_with_tags.map((c, i) => ({
+            ...c,
+            children: JSON.parse(c.children ?? '[]').map(id => children.find(child => child.id === id))
+        }))
 
         if (locals.user) {
             const { user_likes, user_saves } = get_user_likes_and_saves(locals.user.id, content.map(c => c.id))
 
-            content_with_tags = content_with_tags.map((c, i) => ({
+            content_with_tags_and_children = content_with_tags.map((c, i) => ({
                 ...c,
                 liked: user_likes.has(c.id),
                 saved: user_saves.has(c.id)
@@ -25,7 +32,7 @@ export const load = (async ({ url, locals }) => {
 
 
         return {
-            content: content_with_tags,
+            content: content_with_tags_and_children,
         };
     } catch (err) {
         console.error('Error fetching saved content:', err);
