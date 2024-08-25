6d2cdbb6f49861ccb4218a9731d3e81af375b5d9
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2024-08-25 21:04:53 +0200

    Weird workaround to add inline script.

--- sites/www/src/routes/(admin)/admin/statistics/+page.svelte
+++ sites/www/src/routes/(admin)/admin/statistics/+page.svelte
@@ -1,10 +1,20 @@
-<script async src="https://analytics.sveltesociety.dev/js/embed.host.js"></script>
+<script>
+	import { PUBLIC_PLAUSIBLE_SHARED_LINK_AUTH } from '$env/static/public';
 
-<iframe
+	let PLAUSIBLE_HTML =
+		'<script' +
+		`async src="https://analytics.sveltesociety.dev/js/embed.host.js">` +
+		'<' +
+		`
+
+    <iframe
 	title="Svelte Society Analytics"
-	src="https://analytics.sveltesociety.dev/share/sveltesociety.dev?auth=XXX&embed=true&theme=light"
+	src="https://analytics.sveltesociety.dev/share/sveltesociety.dev?auth=${PUBLIC_PLAUSIBLE_SHARED_LINK_AUTH}&embed=true&theme=light"
 	scrolling="no"
 	frameborder="0"
 	loading="lazy"
 	style="width: 1px; min-width: 100%; height: 1600px;"
-></iframe>
+    ></iframe>`;
+</script>
+
+{@html PLAUSIBLE_HTML}
