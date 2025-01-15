f53041218f43bb31fc98a345608c3a4478781fd0
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2025-01-16 09:53:20 +1100

    Update publint script

--- scripts/updatePublint.js
+++ scripts/updatePublint.js
@@ -2,14 +2,11 @@
 // Source: https://github.com/bluwy/publint/blob/master/site/src/utils/worker.js
 
 import { writeFileSync } from 'node:fs';
-import { inflate } from 'pako';
 import getNpmTarballUrl from 'get-npm-tarball-url';
 import { packagesSchema } from '../src/lib/schemas.js';
 import packages from '../src/routes/packages/packages.json' assert { type: 'json' };
 import npm from '../src/lib/data/npm.json' assert { type: 'json' };
 import { publint } from 'publint';
-import { untar } from './untar.js';
-import { createTarballVfs } from './tarball.js';
 import { chunk } from './chunk.js';
 
 /** @param {import('zod').infer<typeof packagesSchema>} input */
@@ -55,26 +52,13 @@ writeFileSync('src/lib/data/publint.json', JSON.stringify(output));
 /** @param {ReturnType<typeof injectVersions>[0]} pkg */
 async function processPackage(pkg) {
 	const tarballUrl = getNpmTarballUrl(pkg.npm, pkg.version);
-	let resultBuffer;
 	try {
 		const result = await fetch(tarballUrl);
-		resultBuffer = await result.arrayBuffer();
+		const resultBuffer = await result.arrayBuffer();
+		const { messages } = await publint({ pack: { tarball: resultBuffer }, level: 'warning' });
+		return { [pkg.npm]: { valid: messages.length === 0 } };
 	} catch (e) {
 		postMessage({ type: 'error', data: 'Package not found' });
 		console.error(e);
 	}
-	let files;
-	try {
-		const tarBuffer = inflate(resultBuffer).buffer; // Handles gzip (gz)
-		files = untar(tarBuffer); // Handles tar (t)
-	} catch (e) {
-		postMessage({ type: 'error', data: 'Failed to unpack package' });
-		console.error(e);
-		return;
-	}
-	const vfs = createTarballVfs(files);
-
-	const pkgDir = files.length ? files[0].name.split('/')[0] : 'package';
-	const { messages } = await publint({ pkgDir, vfs, level: 'warning' });
-	return { [pkg.npm]: { valid: messages.length === 0 } };
 }
