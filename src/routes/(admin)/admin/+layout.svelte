6dc2b42c17be23cb9e5985788397697309babd93
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-03-09 11:17:29 +0100

    Update admin dashboard home link icon

--- src/routes/(admin)/admin/+layout.svelte
+++ src/routes/(admin)/admin/+layout.svelte
@@ -5,7 +5,7 @@ import type { Link } from './types'
 let { data, children } = $props()
 
 const links: Link[] = [
-	{ href: '/admin', label: 'Dashboard', icon: 'M4 6h16M4 12h16M4 18h16' },
+	{ href: '/admin', label: 'Dashboard', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' },
 	{
 		href: '/admin/users',
 		label: 'Users',
