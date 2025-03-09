8a6f2281f879ca8ddc6abb9c01b81ddf4dad714b
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-03-09 20:42:47 +0100

    Show only published content on the dashboard

--- src/routes/(admin)/admin/+page.server.ts
+++ src/routes/(admin)/admin/+page.server.ts
@@ -1,11 +1,11 @@
 import { get_user_count } from '$lib/server/db/user'
-import { get_content_count } from '$lib/server/db/content'
+import { get_published_content_count } from '$lib/server/db/content'
 import { get_moderation_queue_count } from '$lib/server/db/moderation'
 import type { PageServerLoad } from './$types'
 
 export const load = (async () => {
 	const users = get_user_count()
-	const content = get_content_count()
+	const content = get_published_content_count()
 	const moderation_queue = get_moderation_queue_count()
 
 	return {
