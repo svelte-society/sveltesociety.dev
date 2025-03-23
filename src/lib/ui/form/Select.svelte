0908a9cbcacc2d44b7f4d075859fae2155649b0b
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-03-23 18:16:09 +0100

    Add icons and styling to select component

--- src/lib/ui/form/Select.svelte
+++ src/lib/ui/form/Select.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+	import { CaretUpDown, Check } from 'phosphor-svelte'
+	import type { SuperForm } from 'sveltekit-superforms'
+	import { Control, Description, Field, FieldErrors, Label } from 'formsnap'
+	import { getContext } from 'svelte';
+	import { Select } from 'bits-ui';
+
 	interface TextInputProps {
 		label?: string
 		description?: string
@@ -17,11 +23,6 @@
         options
 	}: TextInputProps = $props()
 
-	import type { SuperForm } from 'sveltekit-superforms'
-	import { Control, Description, Field, FieldErrors, Label } from 'formsnap'
-	import { getContext } from 'svelte';
-	import { Select } from 'bits-ui';
-
 	const form: SuperForm<Record<string, string>, any> = getContext('form')
 
     const { form: formData } = form
@@ -39,16 +40,26 @@
 					{label}
 				</Label>
 				<Select.Root type="single" bind:value={$formData[name]} name={props.name}>
-					<Select.Trigger {...props} class="data-fs-error:border-red-300 data-fs-error:bg-red-50 data-fs-error:text-red-600 focus:outline-2 focus:outline-sky-200 w-full rounded-md border-2 border-transparent bg-slate-100 px-2 py-1.5 pr-7 text-sm placeholder-slate-500">
+					<Select.Trigger {...props} class="data-fs-error:border-red-300 items-center data-fs-error:bg-red-50 data-fs-error:text-red-600 focus:outline-2 focus:outline-sky-200 w-full rounded-md border-2 border-transparent bg-slate-100 py-1 pl-2 px-3  text-sm placeholder-slate-500 text-left grid grid-cols-[1fr_auto]">
 						{selectedOptionLabel}
+						<CaretUpDown class="text-gray-500 ml-auto size-4" />
 					</Select.Trigger>
-					<Select.Content>
-						{#each options as option}
-							<Select.Item value={option.value}>
-								{option.label}
-							</Select.Item>
-						{/each}
-					</Select.Content>
+					<Select.Portal>
+						<Select.Content class="focus-override bg-white shadow-2xl outline-hidden z-50 w-[var(--bits-select-anchor-width)] min-w-[var(--bits-select-anchor-width)] select-none rounded-xl px-1 py-3 data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1">
+							{#each options as option}
+								<Select.Item value={option.value} class="rounded-sm data-highlighted:bg-gray-100 outline-hidden data-disabled:opacity-50 flex h-8 w-full select-none items-center py-3 pl-3 pr-1.5 text-sm capitalize">
+									{#snippet children({ selected })}
+										{option.label}
+										{#if selected}
+											<div class="ml-auto">
+											<Check />
+											</div>
+										{/if}
+									{/snippet}
+								</Select.Item>
+							{/each}
+						</Select.Content>
+					</Select.Portal>
 				</Select.Root>
 			{/snippet}
 		</Control>
