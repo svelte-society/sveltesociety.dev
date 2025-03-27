909202ca9fdafdfcc1ef5d165391324679db32ec
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-03-28 00:00:11 +0100

    Adjust markdown editor

--- src/lib/ui/MarkdownEditor.svelte
+++ src/lib/ui/MarkdownEditor.svelte
@@ -1,44 +1,22 @@
 <script lang="ts">
 	import { marked } from 'marked'
-	let { value = $bindable(''), name = '' } = $props()
+	import Textarea from './form/Textarea.svelte'
+	import Toggle from './Toggle.svelte'
+	let { value, name = '' } = $props()
 
-	let tab = $state('write')
+	let preview = $state(false)
 </script>
 
-<div class="w-full">
-	<div class="flex border-b border-gray-200">
-		<button
-			type="button"
-			class={[
-				'border-b-2 px-4 py-2 font-medium text-gray-500 hover:text-gray-700',
-				{ 'border-svelte-900 text-svelte-900 hover:text-svelte-500': tab === 'write' }
-			]}
-			onclick={() => (tab = 'write')}
+<div class="relative">
+	<Textarea {name} placeholder="Enter markdown here" label="Body" rows={20} />
+	{#if preview}
+		<div
+			class="prose absolute top-6 right-0 bottom-4 left-0 max-w-none overflow-y-auto rounded-md border bg-white p-4"
 		>
-			Write
-		</button>
-		<button
-			type="button"
-			class={[
-				'border-b-2 px-4 py-2 font-medium text-gray-500 hover:text-gray-700',
-				{ 'border-svelte-900 text-svelte-900 hover:text-svelte-500': tab === 'preview' }
-			]}
-			onclick={() => (tab = 'preview')}
-		>
-			Preview
-		</button>
-	</div>
-
-	{#if tab === 'write'}
-		<textarea
-			{name}
-			bind:value
-			placeholder="Enter markdown here"
-			class="h-[500px] w-full resize-none bg-slate-100 p-4 font-mono focus:outline-2 focus:outline-sky-200"
-		></textarea>
-	{:else}
-		<div class="prose h-[500px] max-w-none overflow-y-auto rounded-md p-4">
 			{@html marked(value)}
 		</div>
 	{/if}
+	<div class="absolute top-0 right-0">
+		<Toggle text="Preview" name="tab" bind:checked={preview} />
+	</div>
 </div>
---
+++ src/lib/ui/Toggle.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Label, Switch } from 'bits-ui'
+
+	let { text, checked = $bindable(), name } = $props()
+</script>
+
+<div class="flex items-center space-x-3">
+	<Switch.Root
+		bind:checked
+		id="dnd"
+		{name}
+		class="data-[state=unchecked]:shadow-mini-inset data-[state=checked]:bg-svelte-900 peer inline-flex h-[19px] min-h-[19px] w-[44px] shrink-0 cursor-pointer items-center rounded-full px-[3px] transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50  data-[state=unchecked]:bg-gray-200"
+	>
+		<Switch.Thumb
+			class="bg-background data-[state=unchecked]:shadow-mini shadow-popover pointer-events-none block size-[14px] shrink-0 rounded-full bg-white transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0"
+		/>
+	</Switch.Root>
+	<Label.Root for="dnd" class="text-sm font-medium">{text}</Label.Root>
+</div>
