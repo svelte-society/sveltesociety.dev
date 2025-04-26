45d45a25d99b579e6f6ffe39d355ab7496ddedca
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-04-26 15:57:59 +0200

    Enhance Select component to support onchange event and update selected label display

--- src/lib/ui/Select.svelte
+++ src/lib/ui/Select.svelte
@@ -12,16 +12,19 @@
 		name: string
 		selected?: string
 		props?: any
+		onchange?: (value: string) => void
 	}
-	let { options, value = $bindable(), name, selected, props }: Props = $props()
+	let { options, value = $bindable(), name, props, onchange }: Props = $props()
+
+	const selectedLabel = $derived(options.find((option) => option.value === value)?.label)
 </script>
 
-<Select.Root type="single" bind:value {name}>
+<Select.Root type="single" bind:value onValueChange={onchange} {name}>
 	<Select.Trigger
 		{...props}
 		class="grid w-full grid-cols-[1fr_auto] items-center rounded-md border-2 border-transparent bg-slate-100 px-3 py-1 pl-2 text-left text-sm placeholder-slate-500  focus:outline-2 focus:outline-sky-200 data-fs-error:border-red-300 data-fs-error:bg-red-50 data-fs-error:text-red-600"
 	>
-		{selected}
+		{selectedLabel}
 		<CaretUpDown class="ml-auto size-4 text-gray-500" />
 	</Select.Trigger>
 	<Select.Portal>
