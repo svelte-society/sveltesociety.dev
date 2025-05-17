808034d3db3ed3118a919b2fdb395aeabe656bf8
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-05-17 09:09:47 +0200

    Add more tags

--- src/lib/server/db/seeds/seed_tags.ts
+++ src/lib/server/db/seeds/seed_tags.ts
@@ -1,20 +1,29 @@
 import type Database from 'bun:sqlite'
 
-export function seedTags(db: Database.Database) {
+export function seedTags(db: Database) {
 	const insertTagStmt = db.prepare(`
     INSERT INTO tags (name, slug, created_at, updated_at)
     VALUES (?, ?, ?, ?)
   `)
 
 	const tags = [
-		{ name: 'Svelte5', slug: 'svelte-5' },
 		{ name: 'Runes', slug: 'runes' },
 		{ name: 'Utility', slug: 'utility' },
+		{ name: 'State', slug: 'state' },
+		{ name: 'Official', slug: 'official' },
 		{ name: 'Snippet', slug: 'snippet' },
+		{ name: 'Components', slug: 'components' },
+		{ name: 'DevTools', slug: 'devtools' },
+		{ name: 'i18n', slug: 'i18n' },
+		{ name: 'Svelte 5', slug: 'svelte-5' },
+		{ name: 'SvelteKit', slug: 'sveltekit' },
+		{ name: 'Adapter', slug: 'adapter' },
+		{ name: 'Testing', slug: 'testing' },
 		{ name: 'Tutorial', slug: 'tutorial' }
 	]
 
-	const insertTagsTransaction = db.transaction((tags) => {
+	type Tag = { name: string; slug: string }
+	const insertTagsTransaction = db.transaction((tags: Tag[]) => {
 		for (const tag of tags) {
 			const now = new Date().toISOString()
 			insertTagStmt.run(tag.name, tag.slug, now, now)
