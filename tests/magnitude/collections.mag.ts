343d0a4f2bdd5e190e4af39d68e21dece40d1df2
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-06-03 21:21:02 +0200

    Remove magnitude tests

--- tests/magnitude/collections.mag.ts
+++
@@ -1,6 +0,0 @@
-import { test } from 'magnitude-test'
-
-test('view collections', { url: 'http://localhost:5173' })
-	.step('Go to the collections page')
-	.step('Click on the first post in a collection')
-	.check('Should show the post')
--- tests/magnitude/landing-page.mag.ts
+++
@@ -1,5 +0,0 @@
-import { test } from 'magnitude-test'
-
-test('displays posts by most likes', { url: 'http://localhost:5173' })
-	.step('Change sort to most likes')
-	.check('should see posts sorted by most likes')
