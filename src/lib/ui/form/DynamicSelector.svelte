ebb6628dac2e9b0f8f2b4a900834a7961cff5333
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-06-07 18:02:32 +0200

    Updated look for Dynamic Selector component

--- src/lib/ui/form/DynamicSelector.svelte
+++ src/lib/ui/form/DynamicSelector.svelte
@@ -48,55 +48,57 @@
 	}
 </script>
 
-<DynamicInput
-	{name}
-	{label}
-	{description}
-	type="text"
-	{options}
-	placeholder="Select {name}"
-	{Icon}
-/>
+<div class="rounded-md border-2 border-slate-200 p-4">
+	<DynamicInput
+		{name}
+		{label}
+		{description}
+		type="text"
+		{options}
+		placeholder="Select {name}"
+		{Icon}
+	/>
 
-<ul class="space-y-2 rounded-md bg-slate-100 p-4">
-	{#each $formData[name] as item, index (item)}
-		{@const optionItem = options.find((c) => c.value === item)}
-		<li
-			class="flex cursor-grab items-center justify-between gap-2 rounded-md border-2 border-gray-100 bg-white p-2 transition-all duration-300"
-			use:draggable={{
-				container: index.toString(),
-				dragData: item
-			}}
-			use:droppable={{
-				container: index.toString(),
-				callbacks: { onDrop: handleDrop }
-			}}
-			animate:flip={{ duration: 200 }}
-			in:fade={{ duration: 150 }}
-			out:fade={{ duration: 150 }}
-		>
-			<div class="flex items-center gap-2">
-				<ReadCvLogo />
-				{optionItem?.label}
-			</div>
-			<div class="flex items-center gap-2">
-				<DotsNine />
-				<button
-					type="button"
-					class="cursor-pointer"
-					title="Remove {optionItem?.label}"
-					onclick={() => {
-						$formData[name] = $formData[name].filter((i: string) => i !== item)
-					}}
-				>
-					<Trash class="text-red-600" />
-				</button>
-			</div>
-		</li>
-	{:else}
-		<p class="flex items-center gap-2 text-sm text-gray-500">
-			<Empty />
-			<span>No {name} selected</span>
-		</p>
-	{/each}
-</ul>
+	<ul class="space-y-2 rounded-md bg-slate-100 p-4">
+		{#each $formData[name] as item, index (item)}
+			{@const optionItem = options.find((c) => c.value === item)}
+			<li
+				class="flex cursor-grab items-center justify-between gap-2 rounded-md border-2 border-gray-100 bg-white p-2 transition-all duration-300"
+				use:draggable={{
+					container: index.toString(),
+					dragData: item
+				}}
+				use:droppable={{
+					container: index.toString(),
+					callbacks: { onDrop: handleDrop }
+				}}
+				animate:flip={{ duration: 200 }}
+				in:fade={{ duration: 150 }}
+				out:fade={{ duration: 150 }}
+			>
+				<div class="flex items-center gap-2">
+					<ReadCvLogo />
+					{optionItem?.label}
+				</div>
+				<div class="flex items-center gap-2">
+					<DotsNine />
+					<button
+						type="button"
+						class="cursor-pointer"
+						title="Remove {optionItem?.label}"
+						onclick={() => {
+							$formData[name] = $formData[name].filter((i: string) => i !== item)
+						}}
+					>
+						<Trash class="text-red-600" />
+					</button>
+				</div>
+			</li>
+		{:else}
+			<p class="flex items-center gap-2 text-sm text-gray-500">
+				<Empty />
+				<span>No {name} selected</span>
+			</p>
+		{/each}
+	</ul>
+</div>
