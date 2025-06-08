63d651e5a36ffac171b0ef721b23cee83268ff33
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-06-08 21:38:17 +0200

    Add view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script>
 	import '../app.css'
+	import { onNavigate } from '$app/navigation'
 
 	let { children } = $props()
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve()
+				await navigation.complete
+			})
+		})
+	})
 </script>
 
 {@render children()}
