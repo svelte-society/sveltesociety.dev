3b3da460de7e2a47ca1836a95949440e5db781ab
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-06-23 19:20:02 +0200

    Remove unused code in Tag component

--- src/lib/ui/Tag.svelte
+++ src/lib/ui/Tag.svelte
@@ -6,7 +6,6 @@
 
 <script lang="ts">
 	import { page } from '$app/state'
-	import { afterNavigate } from '$app/navigation'
 
 	type Tag = {
 		id: string
@@ -64,20 +63,4 @@
 		/>
 	</svg>
 	{tag.name}
-	{#if onclick}
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="12"
-			height="12"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		>
-			<line x1="18" y1="6" x2="6" y2="18"></line>
-			<line x1="6" y1="6" x2="18" y2="18"></line>
-		</svg>
-	{/if}
 </a>
