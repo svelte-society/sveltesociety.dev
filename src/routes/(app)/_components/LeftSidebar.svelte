866e1464fc653eb3d2219110f6a986c25b7190ce
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-06-23 18:05:26 +0200

    Hide saved link if user is not logged in

--- src/routes/(app)/_components/LeftSidebar.svelte
+++ src/routes/(app)/_components/LeftSidebar.svelte
@@ -36,30 +36,32 @@
 	<nav>
 		<ul class="text-sm font-bold">
 			{#each links as link}
-				{#if link.href}
-					<li
-						title={link.disabled ? 'Please login to view saved content' : ''}
-						class={[
-							{
-								'bg-svelte-500 text-white': page.url.pathname === link.href,
-								'cursor-not-allowed': link.disabled
-							},
-							'w-full rounded-sm px-2 py-0.5'
-						]}
-					>
-						<a
-							class={['block w-full', { 'pointer-events-none text-gray-700': link.disabled }]}
-							href={preserveSearchParams(link.href)}
-							aria-disabled={link.disabled}
+				{#if !link.disabled}
+					{#if link.href}
+						<li
+							title={link.disabled ? 'Please login to view saved content' : ''}
+							class={[
+								{
+									'bg-svelte-500 text-white': page.url.pathname === link.href,
+									'cursor-not-allowed': link.disabled
+								},
+								'w-full rounded-sm px-2 py-0.5'
+							]}
 						>
+							<a
+								class={['block w-full', { 'pointer-events-none text-gray-700': link.disabled }]}
+								href={preserveSearchParams(link.href)}
+								aria-disabled={link.disabled}
+							>
+								{link.name}
+								{link.disabled ? ' (disabled)' : ''}
+							</a>
+						</li>
+					{:else}
+						<li class="mt-2 px-2 py-0.5 text-xs font-thin">
 							{link.name}
-							{link.disabled ? ' (disabled)' : ''}
-						</a>
-					</li>
-				{:else}
-					<li class="mt-2 px-2 py-0.5 text-xs font-thin">
-						{link.name}
-					</li>
+						</li>
+					{/if}
 				{/if}
 			{/each}
 		</ul>
