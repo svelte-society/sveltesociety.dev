ff3b367fbfb4d9b219ced4dd1df93e9b8f9622da
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-10-10 16:46:57 +0200

    Add tracking via Umami

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,4 +16,12 @@
 	})
 </script>
 
+<svelte:head>
+	<script
+		defer
+		src="https://umami.sveltesociety.dev/script.js"
+		data-website-id="d91db9d2-f32f-4781-a861-efba1953edea"
+	></script>
+</svelte:head>
+
 {@render children()}
