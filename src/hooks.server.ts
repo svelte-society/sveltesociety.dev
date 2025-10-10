c71a31faa78e71b922d2b78ba9f53e0c6a644eee
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-10-10 21:22:20 +0200

    Remove seed scripts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,25 +4,9 @@ import { protect_routes } from './hooks/protect_routes'
 import { attach_services } from './hooks/attach_services'
 import type { ServerInit } from '@sveltejs/kit'
 import { initiate_db } from './lib/server/db/initiate'
-import { run_seeds } from './lib/server/db/seeds/index'
-import { dev } from '$app/environment'
-import { hasData } from './lib/server/db/utils'
-import { SEED_DATABASE } from '$env/static/private'
 
 export const init: ServerInit = async () => {
 	initiate_db()
-
-	if (!hasData()) {
-		const seedMode = SEED_DATABASE || (dev ? 'full' : 'minimal')
-		console.log(`Seed mode: ${seedMode}`)
-
-		if (seedMode !== 'none') {
-			run_seeds(seedMode)
-			console.log(`Database seeding completed with mode: ${seedMode}`)
-		} else {
-			console.log('Database seeding disabled by SEED_DATABASE=none')
-		}
-	}
 }
 
 export const handle = sequence(attach_services, add_user_data, protect_routes)
