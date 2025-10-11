72e17d1ce510c672067f0f1532b550bdc2047038
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-10-11 13:53:38 +0200

    Test bun adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from '@sveltejs/adapter-node'
+import adapter from 'svelte-adapter-bun'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
