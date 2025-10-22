b7d5e74a57eb050f672be54282de272ec66559b8
Patrick <Patrick@ShowYou.us>
2025-10-22 14:40:10 +0200

    fix view transition glitch

--- src/routes/(app)/_components/Header.svelte
+++ src/routes/(app)/_components/Header.svelte
@@ -5,7 +5,10 @@
 	let { user, announcement } = $props()
 </script>
 
-<header class="border-svelte-900 sticky top-0 z-10 border-b-4 bg-white py-4">
+<header
+	class="border-svelte-900 sticky top-0 z-10 border-b-4 bg-white py-4"
+	style:view-transition-name="header"
+>
 	<div
 		class="mx-auto grid w-full max-w-[1440px] grid-cols-[auto_1fr_auto] items-center gap-4 px-4 sm:grid-cols-[1.5fr_5fr_2.5fr]"
 	>
@@ -68,7 +71,10 @@
 	</div>
 </header>
 {#if announcement}
-	<div class="bg-svelte-900 sticky top-20 z-10 flex w-full place-content-center p-2">
+	<div
+		class="bg-svelte-900 sticky top-20 z-10 flex w-full place-content-center p-2"
+		style:view-transition-name="announcement"
+	>
 		<a href={announcement.href} class="text-semibold mx-auto text-white underline">
 			{announcement.text}
 		</a>
