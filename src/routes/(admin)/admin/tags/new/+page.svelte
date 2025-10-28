65b427897f91f1d2f8878c8af19cb0568145ed5e
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-10-28 22:28:56 +0100

    Remove generate slug function from create tag page

--- src/routes/(admin)/admin/tags/new/+page.svelte
+++ src/routes/(admin)/admin/tags/new/+page.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
-	import { ADMIN_ROUTES, generateSlug } from '$lib/admin'
+	import { ADMIN_ROUTES } from '$lib/admin'
 	import Button from '$lib/ui/Button.svelte'
 	import { createTag } from '../data.remote'
-
-	function handleGenerateSlug() {
-		const name = createTag.fields.name.value()
-		if (name && !createTag.fields.slug.value()) {
-			createTag.fields.slug.set(generateSlug(name))
-		}
-	}
 </script>
 
 <div class="mx-auto max-w-4xl rounded-lg bg-white p-6 shadow-md">
@@ -29,31 +22,18 @@
 			<p class="text-xs text-gray-500">Enter the name of the tag</p>
 		</div>
 
-		<div class="flex gap-2">
-			<div class="flex flex-1 flex-col gap-2">
-				<label for="slug" class="text-xs font-medium">Slug</label>
-				<input
-					{...createTag.fields.slug.as('text')}
-					id="slug"
-					placeholder="svelte"
-					class="rounded-md border border-gray-300 px-3 py-2 focus:border-blue-500 focus:outline-none"
-				/>
-				{#each createTag.fields.slug.issues() as issue}
-					<p class="text-xs text-red-600">{issue.message}</p>
-				{/each}
-				<p class="text-xs text-gray-500">URL-friendly version of the name</p>
-			</div>
-			<div class="flex items-end pb-0.5">
-				<Button
-					type="button"
-					size="sm"
-					variant="secondary"
-					onclick={handleGenerateSlug}
-					disabled={!createTag.fields.name.value()}
-				>
-					Generate
-				</Button>
-			</div>
+		<div class="flex flex-col gap-2">
+			<label for="slug" class="text-xs font-medium">Slug</label>
+			<input
+				{...createTag.fields.slug.as('text')}
+				id="slug"
+				placeholder="svelte"
+				class="rounded-md border border-gray-300 px-3 py-2 focus:border-blue-500 focus:outline-none"
+			/>
+			{#each createTag.fields.slug.issues() as issue}
+				<p class="text-xs text-red-600">{issue.message}</p>
+			{/each}
+			<p class="text-xs text-gray-500">URL-friendly version of the name</p>
 		</div>
 
 		<div class="mt-6 flex gap-4">
