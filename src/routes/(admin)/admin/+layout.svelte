f999d6aa6c79f0f3d2ce336dab8d82578048d73e
Kevin Åberg Kultalahti <kevin.aberg@gmail.com>
2025-12-03 15:37:48 +0100

    remove preloading from admin dashboard

--- src/routes/(admin)/admin/+layout.svelte
+++ src/routes/(admin)/admin/+layout.svelte
@@ -88,7 +88,10 @@
 	)
 </script>
 
-<div class="flex h-screen bg-gradient-to-br from-gray-50 to-gray-100">
+<div
+	data-sveltekit-preload-data="false"
+	class="flex h-screen bg-linear-to-br from-gray-50 to-gray-100"
+>
 	<Sidebar {links} moderationCount={data.moderation_count} />
 	<div class="flex-1 overflow-y-auto">
 		<div class="mx-auto max-w-7xl p-4 md:p-8">
